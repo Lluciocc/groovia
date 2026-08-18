edf5c246dd4e3747e27d237f9bda836a750ea74f
Lluciocc <llucio.cc00@gmail.com>
2026-08-18 11:40:23 +0200

    ruff checked

--- src/audio/player.py
+++ src/audio/player.py
@@ -1,6 +1,6 @@
 # player.py
 #
-#Copyright 2026 Lluciocc (llucio.cc00@gmail.com)
+# Copyright 2026 Lluciocc (llucio.cc00@gmail.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
